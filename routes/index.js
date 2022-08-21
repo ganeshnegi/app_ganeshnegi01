e4de71d63285b956ae1389ae7ace8723cec31a37
Ganesh Negi <ganesh.negi01@nagarro.com>
2022-08-21 14:42:20 +0530

    separate deployments for branches with load balancers display the configmap and secrets in web app

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render("index", { title: "Express", env: process.env });
+  config = {};
+  if(process.env.STAGE) {
+    config['STAGE'] = process.env.STAGE
+  }
+  if(process.env.APP_LANG) {
+    config['APP_LANG'] = process.env.APP_LANG
+  }
+
+  secrets = {};
+  if(process.env.SECRET_USERNAME) {
+    secrets['SECRET_USERNAME'] = process.env.SECRET_USERNAME
+  }
+  if(process.env.SECRET_PASSWORD) {
+    secrets['SECRET_PASSWORD'] = process.env.SECRET_PASSWORD
+  }
+  res.render("index", {
+    title: "Express",
+    env: process.env,
+    config: config,
+    secrets: secrets
+  });
 });
 
 module.exports = router;
